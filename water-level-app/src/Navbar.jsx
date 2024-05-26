c5b2016345d387c850a0217a78edd3f2fa264844
PaulGaryOca <158600592+PaulGaryOca@users.noreply.github.com>
2024-05-26 16:30:38 +0800

    Update Navbar.jsx

--- water-level-app/src/Navbar.jsx
+++ water-level-app/src/Navbar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <nav>
       <ul style={{ listStyleType: 'none', padding: 0, display: 'flex', gap: '10px', justifyContent: 'flex-end',marginRight:'50px'   ,marginTop:'50px'    }}>
       <li><Link to="/">Home</Link></li>
-     
+     //useless
       </ul>
     </nav>
   );
